86713bfbaee8e582aea4d51cc91a5aba69d0c3c7
wen-fei <282699766@qq.com>
2018-04-20 22:50:29 +0800

    tika tool for pdf

--- AbstractExe/Tika/tikaTool.py
+++ AbstractExe/Tika/tikaTool.py
@@ -13,6 +13,7 @@ tika.initVM()
 from tika import parser
 ##########################################
 import re
+import os
 
 def get_pdf_content(fileName):
     """
@@ -36,37 +37,46 @@ def get_abstrct_by_re(content):
      # 去除字符串中的换行符
      content = content.replace("\r\n", "")
      content = content.replace("\n", "")
+     content = content.lower()
      # 匹配规则1：标准的<p>Abstarct</p><p>...</p>
-     patterns = re.compile(r'\D+<p>(.*?)</p><p>(.*?)</p>')
-     match = patterns.match(content)
-     if match:
-          return match.group(2)
+     pattern1 = re.compile(r'\D+<p>[Aa]bstract</p><p>(.*?)</p>')
+     match1 = pattern1.match(content)
+     if match1:
+          return match1.group(1)
      else:
-          return "null"
+          pattern2 = re.compile(r'.*<p>abstract(.*?)introduction')
+          match2 = pattern2.match(content)
+          if match2:
+              return match2.group(1)
+          else:
+              return None
 
-test_str = """
-</p>
-<p>Abstract
-</p>
-<p>Background: Medical concepts are inherently ambiguous and error-prone due to human fallibility, which makes it hard for
-them to be fully used by classical machine learning methods (eg, for tasks like early stage disease prediction).
-Objective: Our work was to create a new machine-friendly representation that resembles the semantics of medical concepts.
-We then developed a sequential predictive model for medical events based on this new representation.
-Methods: We developed novel contextual embedding techniques to combine different medical events (eg, diagnoses, prescriptions,
-and labs tests). Each medical event is converted into a numerical vector that resembles its “semantics,” via which the similarity
-between medical events can be easily measured. We developed simple and effective predictive models based on these vectors to
-predict novel diagnoses.
-Results: We evaluated our sequential prediction model (and standard learning methods) in estimating the risk of potential
-diseases based on our contextual embedding representation. Our model achieved an area under the receiver operating characteristic
-(ROC) curve (AUC) of 0.79 on chronic systolic heart failure and an average AUC of 0.67 (over the 80 most common diagnoses)
-using the Medical Information Mart for Intensive Care III (MIMIC-III) dataset.
-Conclusions: We propose a general early prognosis predictor for 80 different diagnoses. Our method computes numeric
-representation for each medical event to uncover the potential meaning of those events. Our results demonstrate the efficiency
-of the proposed method, which will benefit patients and physicians by offering more accurate diagnosis.
-</p>
 
-"""
-print(get_abstrct_by_re(test_str))
+
+file_path = "D:\\论文库\\知识图谱\\数据集\\2016 4-9"
+listfiles = os.listdir(file_path)
+errorFiles = []
+for file in listfiles:
+    # 判断文件类型
+    file_ex = file.split(".")[1]
+    if file_ex.lower() != 'pdf':
+        errorFiles.append(file)
+        print(file + '--------------------文件类型错误--------------->')
+    else:
+        content = get_pdf_content(os.path.join(file_path, file))
+        if content[0] is None:
+            errorFiles.append(file)
+            print(file + '--------------------解析失败--------------->')
+        else:
+            abs_res = get_abstrct_by_re(content[0])
+            if abs_res is None:
+                errorFiles.append(file)
+                print(file + '--------------------解析失败--------------->')
+            else:
+                print("得到Abstract======================>" + abs_res)
+                print(file + '--------------------解析完成--------------->')
+acc = 1- len(errorFiles) / len(listfiles)
+print("提取准确率为：", acc)
 # FILE_PATH = "../data/test1.pdf"
 # content, _ = get_pdf_content(fileName=FILE_PATH)
 # print(content)
